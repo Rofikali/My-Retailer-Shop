da784203af9ca0f5d25db3fa8f634c9306d36157
Rofik Don <alirofikr@gmail.com>
2026-08-30 12:14:49 +0530

    Shorten receipt allocation constraint name

--- server/db/migrations/0011_customer_receivable_settlement.sql
+++ server/db/migrations/0011_customer_receivable_settlement.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS "customer_receipt_allocations" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
-  ALTER TABLE "customer_receipt_allocations" ADD CONSTRAINT "customer_receipt_allocations_receipt_event_id_party_ledger_events_id_fk" FOREIGN KEY ("receipt_event_id") REFERENCES "public"."party_ledger_events"("id") ON DELETE restrict ON UPDATE no action;
+  ALTER TABLE "customer_receipt_allocations" ADD CONSTRAINT "receipt_allocations_receipt_event_fk" FOREIGN KEY ("receipt_event_id") REFERENCES "public"."party_ledger_events"("id") ON DELETE restrict ON UPDATE no action;
 EXCEPTION WHEN duplicate_object THEN NULL;
 END $$;
 --> statement-breakpoint
